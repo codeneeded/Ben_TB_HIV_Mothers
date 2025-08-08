1c130631e4616e1de987e6977cc8a2cac6d679e2
Akshay Iyer <ammasakshay@gmail.com>
2025-08-08 16:56:38 -0400

    Updated Annotation Genes

--- Scripts/5.Annotation.R
+++ Scripts/5.Annotation.R
@@ -44,25 +44,29 @@ seu_parsef_integrated <- subset(seu_parsef_integrated, idents = large_clusters)
 # Marker List
 # ---------------------------- #
 rna.features <- c(
-  'ASCL2','BATF','BCL6','C1QBP','CCL2','CCL3','CCL4L2','CCL5','CCND3','CD14','CD19','CD200','CD27',
-  'CD3D','CD3E','CD36','CD4','CD40','CD40LG','CD70','CD7','CD79A','CD8A','CD8B','CTLA4','CTSW',
-  'CXCL8','CXCR3','CXCR5','EBI3','FABP5','FCGR2B','FCGR3A','FOXP3','GNLY','GZMK','HAVCR2',
-  'HIST1H4C','HLA-DPA1','HLA-DRA','HLA-DRB1','ICOS','IFI30','IFNG','IGFBP2','IGFBP4','IGHM',
-  'IKZF2','IL10','IL18BP','IL1B','IL21','IL2RA','IL2RB','IRF4','IRF8','ITGAX','KLRD1','KLRC1',
-  'LDHA','LGALS1','LTA','LTB','MAF','MAL','MALAT1','MIR155HG','MKI67','MT-ND1','MT-ND5','MS4A1',
-  'NELL2','NCAM1','NKG7','NR4A1','PDCD1','PRDM1','PRF1','SELL','SERPINA1','SERPING1','SH2D1A',
-  'TCF7','TIGIT','TNF','TNFAIP2','TNFRSF18','TNFRSF4','TNFRSF9','TOX','TRBC1','TRDV1','TRDV2',
-  'TRGC2','TRGV9','XCL1','XCL2',
-  'TBX21','GATA3','RORC','IL17A','LAG3','ENTPD1','BATF3','XBP1','JCHAIN','CLEC9A','CD1C','TCF4','HIF1A'
+  'ASCL2','BATF','BATF3','BCL6','C1QBP','CCL2','CCL3','CCL4L2','CCL5','CCND3','CD14','CD19','CD1C',
+  'CD200','CD27','CD3D','CD3E','CD36','CD4','CD40','CD40LG','CD70','CD7','CD79A','CD8A','CD8B',
+  'CLEC9A','CR2','CTLA4','CTSW','CXCL8','CXCR3','CXCR5','EBI3','ENTPD1','FABP5','FCGR2B','FCGR3A',
+  'FCRL5','FOXP3','GNLY','GP1BA','GP9','GATA3','GZMK','HAVCR2','HIF1A','HIST1H4C','HLA-DPA1',
+  'HLA-DRA','HLA-DRB1','ICOS','IFI30','IFNG','IGFBP2','IGFBP4','IGHA1','IGHA2','IGHG1','IGHG2','IGHG3','IGHG4','IGHM','IKZF2','IL10',
+  'IL17A','IL18BP','IL18RAP','IL1B','IL21','IL2RA','IL2RB','IRF4','IRF8','ITGAX','JCHAIN','KLRB1',
+  'KLRD1','KLRC1','LAG3','LDHA','LGALS1','LTA','LTB','MAF','MAL','MALAT1','MIR155HG','MKI67',
+  'MT-ND1','MT-ND5','MS4A1','NELL2','NCAM1','NKG7','NR4A1','PDCD1','PF4','PPBP','PRDM1','PRF1',
+  'RORC','SELL','SERPINA1','SERPING1','SH2D1A','TCF4','TCF7','TIGIT','TNF','TNFAIP2','TNFRSF18',
+  'TNFRSF4','TNFRSF9','TOX','TBX21','TRBC1','TRDC','TRDV1','TRDV2','TRGC1','TRGC2','TRGV9','XBP1',
+  'XCL1','XCL2','ZBTB16','ZEB2'
 )
+
 # ---------------------------- #
 # Generate Plots for Each Marker
 # ---------------------------- #
 
-
 # Feature Plots
 for (gene in rna.features) {
-  if (gene %in% rownames(seu_parsef_integrated)) {
+  feature_file <- file.path(featureplot_dir, paste0("Feature_", gene, ".png"))
+  
+  # Skip if plot already exists
+  if (!file.exists(feature_file) && gene %in% rownames(seu_parsef_integrated)) {
     p_feat <- DimPlot2(
       seu_parsef_integrated,
       features = gene,
@@ -71,7 +75,7 @@ for (gene in rna.features) {
     )
     
     ggsave(
-      filename = file.path(featureplot_dir, paste0("Feature_", gene, ".png")),
+      filename = feature_file,
       plot = p_feat,
       width = 10,
       height = 8,
@@ -82,7 +86,10 @@ for (gene in rna.features) {
 
 # Violin Plots
 for (gene in rna.features) {
-  if (gene %in% rownames(seu_parsef_integrated)) {
+  vln_file <- file.path(vlnplot_dir, paste0("Vln_", gene, ".png"))
+  
+  # Skip if plot already exists
+  if (!file.exists(vln_file) && gene %in% rownames(seu_parsef_integrated)) {
     p_vln <- VlnPlot2(
       seu_parsef_integrated,
       features = gene,
@@ -91,7 +98,7 @@ for (gene in rna.features) {
     ) 
     
     ggsave(
-      filename = file.path(vlnplot_dir, paste0("Vln_", gene, ".png")),
+      filename = vln_file,
       plot = p_vln,
       width = 10,
       height = 8,
@@ -100,6 +107,10 @@ for (gene in rna.features) {
   }
 }
 
+
+DotPlot2(seu_parsef_integrated, features = c("IFNG","TNF","TIGIT","LAG3","CD4","CD8A"),split.by = "IGRA_status")
+
+FeaturePlot3(seu_parsef_integrated, color = "ryb", feature.1 = "CD4", feature.2 = "CD8A", feature.3 = "CD8B", pt.size = 0.5,reduction = "umap.mnn.rna")
 # ---------------------------- #
 # Save Post-Subset Object
 # ---------------------------- #
