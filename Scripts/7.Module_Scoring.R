cdffea9a1a2694d365f4479438300f49403bb32f
Akshay Iyer <ammasakshay@gmail.com>
2026-02-03 09:58:24 -0500

    VLN Plots - Annotated

--- Scripts/7.Module_Scoring.R
+++ Scripts/7.Module_Scoring.R
@@ -992,3 +992,64 @@ plot_module_summary <- function(stats_df, out_dir) {
 
 # Run it
 plot_module_summary(stats_df, out_dir)
+
+
+############################# Post Annotation VLN Plots ###################
+base_dir <- "/home/akshay-iyer/Documents/Ben_TB_HIV_Mothers"
+annotation_dir <- file.path(base_dir, "Annotation")
+vlnplot_dir <- file.path(annotation_dir, "Post-Annotation/VlnPlots")
+dir.create(vlnplot_dir, showWarnings = FALSE)
+
+igra_order <- c(
+  "CD4+ naïve","CD4+ Activated","CD4+ Tfh","CD4+ Treg",
+  "CD8+ TCM","CD8+ EM","CD8+ TEMRA",
+  "dnT",
+  "NKCD56bright","NK Cytotoxic",
+  "cDC2","moDC precursor","moDC","DC-like APCs",
+  "CD14+ Monocytes","CD16+ NCM",
+  "Naïve B","Memory B","Intermediate B","GC-like B"
+)
+seu$IGRA_Annotation <- factor(seu$IGRA_Annotation, levels = igra_order)
+
+
+
+# ---------------------------- #
+# Marker List
+# ---------------------------- #
+rna.features <- c(
+  'ASCL2','BATF','BATF3','BCL6','C1QBP','CCL2','CCL3','CCL4L2','CCL5','CCND3','CD14','CD19','CD1C',
+  'CD200','CD27','CD3D','CD3E','CD36','CD4','CD40','CD40LG','CD70','CD7','CD79A','CD8A','CD8B',
+  'CLEC9A','CR2','CTLA4','CTSW','CXCL8','CXCR3','CXCR5','EBI3','ENTPD1','FABP5','FCGR2B','FCGR3A',
+  'FCRL5','FOXP3','GNLY','GP1BA','GP9','GATA3','GZMK','HAVCR2','HIF1A','HIST1H4C','HLA-DPA1',
+  'HLA-DRA','HLA-DRB1','ICOS','IFI30','IFNG','IGFBP2','IGFBP4','IGHA1','IGHA2','IGHG1','IGHG2','IGHG3','IGHG4','IGHM','IKZF2','IL10',
+  'IL17A','IL18BP','IL18RAP','IL1B','IL21','IL2RA','IL2RB','IRF4','IRF8','ITGAX','JCHAIN','KLRB1',
+  'KLRD1','KLRC1','LAG3','LDHA','LGALS1','LTA','LTB','MAF','MAL','MALAT1','MIR155HG','MKI67',
+  'MT-ND1','MT-ND5','MS4A1','NELL2','NCAM1','NKG7','NR4A1','PDCD1','PF4','PPBP','PRDM1','PRF1',
+  'RORC','SELL','SERPINA1','SERPING1','SH2D1A','TCF4','TCF7','TIGIT','TNF','TNFAIP2','TNFRSF18',
+  'TNFRSF4','TNFRSF9','TOX','TBX21','TRBC1','TRDC','TRDV1','TRDV2','TRGC1','TRGC2','TRGV9','XBP1',
+  'XCL1','XCL2','ZBTB16','ZEB2'
+)
+for (gene in rna.features) {
+  vln_file <- file.path(vlnplot_dir, paste0("Vln_", gene, ".png"))
+  
+  # Skip if plot already exists
+  if (!file.exists(vln_file) && gene %in% rownames(seu)) {
+    p_vln <- VlnPlot2(
+      seu,
+      features = gene,
+      show.mean = TRUE,
+      mean_colors = c("red", "blue")
+    ) +
+      theme(plot.margin = unit(c(1, 1, 1, 2), "cm")) # Increase left margin (4th value)
+    
+    ggsave(
+      filename = vln_file,
+      plot = p_vln,
+      width = 10,
+      height = 8,
+      dpi = 300
+    )
+  }
+}
+table(seu$sample)
+table(seu$IGRA_status)
